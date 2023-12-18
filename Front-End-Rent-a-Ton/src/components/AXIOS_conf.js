fbff98fba3d7fb9a8091f3ba345a1e23e5dd3959
mojoke7 <147321193+mojoke7@users.noreply.github.com>
2023-12-18 13:31:22 +0000

    Delete Front-End-Rent-a-Ton directory

--- Front-End-Rent-a-Ton/src/components/AXIOS_conf.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-//Setting the base URL of the Django server so we won't have to specify it in every axios request.
-export default axios.create({
-  baseURL: `http://localhost:8000/`
-});
